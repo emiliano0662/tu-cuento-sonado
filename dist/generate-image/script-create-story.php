a5f330d8378b111d4a1bffa41b618fc7d36894f5
Emiliano Carreño <emiliano0662@gmail.com>
2018-09-04 00:25:55 -0500

    cambios generales

---
+++ dist/generate-image/script-create-story.php
@@ -0,0 +1,43 @@
+<?php
+
+$get_type = (empty($_GET['type']))? 'girl' : $_GET['type'];
+$get_skin = (empty($_GET['skin']))? '1' : $_GET['skin'];
+$get_eyes = (empty($_GET['eyes']))? '1' : $_GET['eyes'];
+$get_hair = (empty($_GET['hair']))? '1' : $_GET['hair'];
+
+$img = imagecreatetruecolor(230, 200);
+
+// Hacer que los canales alfa funcionen
+imagealphablending($img, true);
+imagesavealpha($img, true);
+
+$trans_bg = imagecolorallocatealpha($img, 0, 0, 0, 127);
+
+imagefill($img, 0, 0, $trans_bg);
+
+// Cargar imagen Piel
+$cur_skin = imagecreatefrompng("./images/".$get_type."/skin/".$get_skin.".png");
+// Cargar imagen Ojos
+$cur_eyes = imagecreatefrompng("./images/".$get_type."/eyes/".$get_eyes.".png");
+// Cargar imagen Cabello
+$cur_hair = imagecreatefrompng("./images/".$get_type."/hair/".$get_hair.".png");
+
+// Copia sobre la imagen Piel
+imagecopy($img, $cur_skin, 0,0,0,0, 230, 200);
+// Copia sobre la imagen Ojos
+imagecopy($img, $cur_eyes, 0,0,0,0, 230, 200);
+// Copia sobre la imagen Cabello
+imagecopy($img, $cur_hair, 0,0,0,0, 230, 200);
+
+// Liberar memoria
+imagedestroy($cur_skin);
+imagedestroy($cur_eyes);
+imagedestroy($cur_hair);
+
+header('Content-Type: image/png');
+
+imagepng($img);
+
+imagedestroy($img);
+
+?>
--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,153 @@
 $(document).ready(function() {
 
+	var get_type = 'girl';
+	var get_skin = 1;
+	var get_eyes = 1;
+	var get_hair = 1;
+
+	$.validate({
+		form: '#form-crear-cuento',
+	});
+
+	$("input[name=gender]").change(function () {
+
+		get_type = $(this).val();
+
+		$("#img-select-avatar").attr("src", "./generate-image/script-avatar.php?type="+get_type+"&skin="+get_skin+"&eyes="+get_eyes+"&hair="+get_hair);
+
+	});
+
+	$(".btn-change-create-story").on('click', function (event) {
+		event.preventDefault();
+
+		var item = $(this).data('item');
+
+		switch (item) {
+			case 2:
+			
+				$('#name').validate(function (valid, elem) {
+
+					if (valid) {
+
+						$(".create-story-block").hide();
+						$(".container-create-story-"+item).fadeIn();
+					}
+				});
+
+			break;
+			case 5:
+
+				$(".create-story-block").hide();
+				$(".container-create-story-2").fadeIn();
+
+			break;
+		
+			default:
+
+				$(".create-story-block").hide();
+				$(".container-create-story-"+item).fadeIn();
+
+			break;
+		}
+
+	});
+
+	$(".carousel-create-story-items").on('click', '.carousel-item', function (event) {
+		event.preventDefault();
+
+		var item = $(this).data('item');
+		var type = $(this).data('type');
+
+		$(".content-items-"+item+" .carousel-item").removeClass('active');
+		$(this).addClass('active');
+
+		switch (item) {
+			case 'skin':
+				get_skin = type;
+				$('#hidde_value_skin').val(type);
+				break;
+			case 'ayes':
+				get_eyes = type;
+				$('#hidde_value_eyes').val(type);
+				break;
+			case 'hair':
+				get_hair = type;	
+				$('#hidde_value_hair').val(type);
+				break;
+		}
+
+		$("#img-select-avatar").attr("src", "./generate-image/script-avatar.php?type="+get_type+"&skin="+get_skin+"&eyes="+get_eyes+"&hair="+get_hair);
+
+	});
+
+	$("#form-crear-cuento").submit(function (event) {
+		event.preventDefault();
+
+		$.ajax({
+			cache: false,
+			type: $(this).attr("method"),
+			url: $(this).attr("action"),
+			data: $(this).serialize(),
+			success: function (data) {
+
+			}
+		});
+
+	});
+
+	$("#croppie-img-file").on('change', function (event) {
+		event.preventDefault();
+
+		if (this.files[0]) {
+
+			var reader = new FileReader();
+
+			reader.onload = function (event) {
+
+				image = event.target.result;
+
+				$("#upload-croppie").croppie('destroy');
+
+				$("#upload-croppie").croppie({
+					viewport: {
+						width: 150,
+						height: 150,
+					}
+				});
+
+				$("#upload-croppie").croppie('bind', image);
+			}
+
+			reader.readAsDataURL(this.files[0]);
+
+		} else {
+
+			alert('Lo sentimos - tu navegador no es compatible con la API FileReader');
+		}
+
+	});
+
+	$("#btn-croppie-file-add").on('click', function (event) {
+		event.preventDefault();
+
+		$("#croppie-img-file").click();
+
+	});
+
+	$("#btn-croppie-file-accept").on('click', function (event) {
+		event.preventDefault();
+
+		$("#upload-croppie").croppie('result', {
+			type: 'canvas',
+			size: 'viewport'
+		}).then(function (resp) {
+
+			$("#hidden_croppie_img_file").val(resp);
+
+		});
+
+	});
+
 	var owl_carousel_main = $('.owl-carousel-main').owlCarousel({
 		items: 1,
 		margin: 0,
@@ -29,36 +177,6 @@ $(document).ready(function() {
 		$('#input-create-story-language').val(language);
 	});
 
-	$("#form-contact").submit(function (event) {
-		event.preventDefault();
-
-		$.ajax({
-			cache: false,
-			type: $(this).attr("method"),
-			url: $(this).attr("action"),
-			data: $(this).serialize(),
-			success: function (data) {
-
-				if (data) {
-
-					$('.alert').hide();
-					$('.alert-success').fadeIn();
-
-					$("#form-contact")[0].reset();
-					
-				}else{
-
-					$('.alert').hide();
-					$('.alert-danger').fadeIn();
-
-				}
-
-				setTimeout(function () { $('.alert').hide(); }, 5000);
-			}
-		});
-
-	});
-
 	var owl_carousel_create_story = $('.carousel-create-story-items').owlCarousel({
 		items: 1,
 		margin: 0,
@@ -78,17 +196,6 @@ $(document).ready(function() {
 		event.preventDefault();
 
 		owl_carousel_create_story.trigger('next.owl.carousel');
-	});
-
-
-	$(".btn-change-create-story").on('click', function (event) {
-		event.preventDefault();
-
-		var item = $(this).data('item');
-
-		$(".create-story-block").hide();
-		$(".container-create-story-" + item).fadeIn();
-		
 	});
 
 	$("[data-dinaanim]").each(function () {
